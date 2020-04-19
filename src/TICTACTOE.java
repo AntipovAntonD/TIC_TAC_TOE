8bbb0eb68487ac69e4c5f9591a0a15288acbf32a
Antipov Anton <antipovantond@gmail.com>
2020-04-19 17:23:26 +0300

    Computer became cleverer. He will try to close your win conditions.

--- src/TICTACTOE.java
+++ src/TICTACTOE.java
@@ -4,12 +4,15 @@ import java.util.Scanner;
 
 public class TICTACTOE {
 
+    //VARIABLES
     public static char[][] map;
     public static final int SIZE = 3;
     public static final char DOT_EMPTY = '*';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
     public static int COUNTER_OF_TURNS = 1;
+
+    public enum Type {MAIN_DIAGONAL, DIAGONAL, ROW, COLUMN}
 
     // POPULATE THE MAP WITH DOT_EMPTY
     public static void initMap() {
@@ -57,14 +60,13 @@ public class TICTACTOE {
 
     //CHECK OF WIN
     public static boolean checkOfWin() {
-        boolean flag = false;
-
         String checkLineX = "";
         String checkLineO = "";
         String diagonal1 = "";
         String diagonal2 = "";
         String row = "";
         String column = "";
+        String draw = "";
 
         //LINE FOR WIN
         for (int i = 0; i < map.length; i++) {
@@ -80,7 +82,7 @@ public class TICTACTOE {
 
         if (checkLineX.equals(diagonal1) || checkLineX.equals(diagonal2) ||
                 checkLineO.equals(diagonal1) || checkLineO.equals(diagonal2)) {
-            flag = true;
+            return true;
         }
 
         //ROWS AND COLUMNS
@@ -88,18 +90,24 @@ public class TICTACTOE {
             for (int j = 0; j < map.length; j++) {
                 row += map[i][j];
                 column += map[j][i];
+                draw += map[i][j];
             }
 
             if (checkLineX.equals(row) || checkLineX.equals(column) ||
                     checkLineO.equals(row) || checkLineO.equals(column)) {
-                flag = true;
+                return true;
             }
 
             row = "";
             column = "";
         }
 
-        return flag;
+        if (!draw.contains(Character.toString(DOT_EMPTY))) {
+            COUNTER_OF_TURNS = -1;
+            return true;
+        }
+
+        return false;
     }
 
     //SET Xs AND Os
@@ -110,58 +118,169 @@ public class TICTACTOE {
         if (x >= 0 && x < SIZE && y >= 0 && y < SIZE) {
             map[x][y] = mark;
         } else {
-            System.out.println("Вы ввели числа за пределами поля");
+            System.out.println("Point outside the grid");
         }
+    }
+
+    //CHECK POSSIBILITY OF WIN
+    public static int[] checkWinCondition(String lineOfMap, Type type, int row) {
+        int cntX = 0;
+        int cntO = 0;
+        int[] point = new int[2];
+
+        for (char c :
+                lineOfMap.toCharArray()) {
+            if (c == DOT_X) {
+                cntX += 1;
+            } else if (c == DOT_O) {
+                cntO += 1;
+            }
+        }
+
+         if ((cntX == SIZE - 1 || cntO == SIZE - 1) && lineOfMap.contains(Character.toString(DOT_EMPTY))) {
+            if (type == Type.MAIN_DIAGONAL) {
+                point[0] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+                point[1] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+            } else if (type == Type.DIAGONAL) {
+                if (lineOfMap.indexOf(Character.toString(DOT_EMPTY)) == 0) {
+                    point[0] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+                    point[1] = SIZE;
+                } else if (lineOfMap.indexOf(Character.toString(DOT_EMPTY)) == 1) {
+                    point[0] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+                    point[1] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+                } else {
+                    point[0] = SIZE;
+                    point[1] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) - 1;
+                }
+            } else if (type == Type.ROW) {
+                point[0] = row + 1;
+                point[1] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+            } else if (type == Type.COLUMN) {
+                point[0] = lineOfMap.indexOf(Character.toString(DOT_EMPTY)) + 1;
+                point[1] = row + 1;
+            }
+        }
+
+        return point;
+    }
+
+    //HAVE TO STOP HUMAN
+    public static int[] checkHumanTurn() {
+        int[] point;
+        String diagonalMain = "";
+        String diagonal = "";
+        String row = "";
+        String column = "";
+
+        //DIAGONALS
+        for (int i = 0; i < map.length; i++) {
+            diagonalMain += map[i][i];
+            diagonal += map[i][map.length - i - 1];
+        }
+
+        point = checkWinCondition(diagonalMain, Type.MAIN_DIAGONAL, 0);
+        if (point[0] != 0 && point[1] != 0) {
+            return point;
+        }
+
+        point = checkWinCondition(diagonal, Type.DIAGONAL, 0);
+        if (point[0] != 0 && point[1] != 0) {
+            return point;
+        }
+
+        //ROWS AND COLUMNS
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map.length; j++) {
+                row += map[i][j];
+                column += map[j][i];
+            }
+
+            point = checkWinCondition(row, Type.ROW, i);
+
+            if (point[0] != 0 && point[1] != 0) {
+                return point;
+            }
+
+            point = checkWinCondition(column, Type.COLUMN, i);
+
+            if (point[0] != 0 && point[1] != 0) {
+                return point;
+            }
+
+            row = "";
+            column = "";
+        }
+
+        return point;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x, y;
-        System.out.println("Это игра крестики-нолики. Вы ходите первым и играете крестиками (X).\n" +
-                "Компьютер ходит вторым и играет ноликами (O).\n" +
-                "Для того, чтобы сделать ход нужно ввести номер столбца и номер строки. Пример: 2 2.\n\n");
+        String continueToPlay = "Y";
 
-        initMap();
-        printMap();
+        System.out.println("This is game TIC TAC TOE. You go first and play crosses (X).\n" +
+                "The computer goes second and plays zeroes (O).\n" +
+                "In order to make a move, you need to enter the column number and row number through a space. Sample: 2 2.\n\n");
 
-        while (!checkOfWin()) {
-            //HUMAN TURN
-            if (COUNTER_OF_TURNS % 2 == 1) {
-                System.out.print("Ваш ход: ");
-
-                x = sc.nextInt();
-                y = sc.nextInt();
-
-                if (isCellEmpty(x, y)) {
-                    setXO(x, y, DOT_X);
-                } else {
-                    System.out.println("Ячейка занята");
-                    continue;
-                }
-
-            }
-            // COMPUTER TURN
-            else {
-                x = (int) (1 + Math.random() * SIZE);
-                y = (int) (1 + Math.random() * SIZE);
-
-                if (isCellEmpty(x, y)) {
-                    setXO(x, y, DOT_O);
-                } else {
-                    continue;
-                }
-            }
-
-            System.out.println();
-
+        while (continueToPlay.equals("Y")) {
+            initMap();
             printMap();
-            COUNTER_OF_TURNS++;
-        }
 
-        if (COUNTER_OF_TURNS % 2 == 1) {
-            System.out.println("Компьтер победил");
-        } else {
-            System.out.println("Вы победили");
+            while (!checkOfWin()) {
+                //HUMAN TURN
+                if (COUNTER_OF_TURNS % 2 == 1) {
+                    System.out.print("Your turn: ");
+
+                    x = sc.nextInt();
+                    y = sc.nextInt();
+
+                    if (isCellEmpty(x, y)) {
+                        setXO(x, y, DOT_X);
+                    } else {
+                        System.out.println("Cell is not empty");
+                        continue;
+                    }
+                }
+                // COMPUTER TURN
+                else {
+                    int[] checkPoint = checkHumanTurn();
+
+                    if (checkPoint[0] == 0 && checkPoint[1] == 0) {
+                        x = (int) (1 + Math.random() * SIZE);
+                        y = (int) (1 + Math.random() * SIZE);
+                    } else {
+                        x = checkPoint[0];
+                        y = checkPoint[1];
+                    }
+
+                    if (isCellEmpty(x, y)) {
+                        setXO(x, y, DOT_O);
+                    } else {
+                        continue;
+                    }
+                }
+
+                System.out.println();
+
+                printMap();
+                COUNTER_OF_TURNS++;
+            }
+
+            if (COUNTER_OF_TURNS % 2 == 1) {
+                System.out.println("You lost");
+            } else if (COUNTER_OF_TURNS % 2 == 0) {
+                System.out.println("You won");
+            } else {
+                System.out.println("Draw");
+            }
+
+            System.out.println("Press [Y] to play one more time, or press [N] to exit.");
+            continueToPlay = sc.next();
+
+            if (continueToPlay.equals("Y")) {
+                COUNTER_OF_TURNS = 1;
+            }
         }
     }
 }
